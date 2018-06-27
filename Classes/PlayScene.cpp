ebe16a1ad6882d989878871fc8cb9ae35ee9eba4
YoHL <hhl@YoHL.com>
2018-06-27 22:24:21 +0800

    更新文本框

--- Classes/PlayScene.cpp
+++ Classes/PlayScene.cpp
@@ -20,6 +20,21 @@ bool PlayScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+    auto background_image = Sprite::create("/home/hhl/cocos/CocosGame/Resources/Q版坦克素材/bg_new.jpg");
+    if (background_image == nullptr)
+    {
+        return false;
+     
+    }
+    else
+    {
+        background_image->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
+        //background_image->setScale(20);
+        this->addChild(background_image);
+    }
+
+
+
     
     return true;
 }
