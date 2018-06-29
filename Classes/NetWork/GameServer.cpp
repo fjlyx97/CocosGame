c8828e0b50c77ec5ac00f23b83612ac54d492f62
lyx <fjlyx97@outlook.com>
2018-06-29 20:32:15 +0800

    更新网络服务端模块

--- Classes/NetWork/GameServer.cpp
+++ Classes/NetWork/GameServer.cpp
@@ -1,4 +1,6 @@
 #include "GameServer.h"
+#include <thread>
+#include <cstring>
 
 bool GameServer::init()
 {
@@ -12,14 +14,40 @@ GameServer::GameServer()
     this->mSocket = new ODSocket();
     this->mSocket->Init();
     this->mSocket->Create(AF_INET, SOCK_STREAM , 0);
-    //this->mSocket->Bind(port);
-    if (this->mSocket->Listen(6))
+    this->mSocket->Bind(this->port);
+    if (!(this->mSocket->Listen(6)))
     {
+        log("监听端口失败");
+    }
+    while (true)
+    {
+        ODSocket* clientSocket = new ODSocket;
+        if (mSocket->Accept(*clientSocket,ip))
+        {
+            connectSocket.push_back(clientSocket);
+            std::thread recvThread(GameServer::recvGameMsg,clientSocket);
+            recvThread.detach();
+        }
+        //此处似乎有内存泄露
+    }
+}
 
+void GameServer::recvGameMsg(ODSocket * clientSocket)
+{
+    char message[1024];
+    while (true)
+    {
+        memset(message,0,sizeof(message));
+        clientSocket->Recv(message,1024,0);
+        log("%s",message);
     }
 }
 
 GameServer::~GameServer()
 {
     this->mSocket->Clean();
+    for (auto socket : connectSocket)
+    {
+        delete socket;
+    }
 }
--- Classes/NetWork/GameServer.h
+++ Classes/NetWork/GameServer.h
@@ -2,6 +2,7 @@
 #define _GameServer_H_
 #include "cocos2d.h"
 #include "ODSocket/ODSocket.h"
+#include <vector>
 #include <string>
 USING_NS_CC;
 class GameServer : public Ref
@@ -11,11 +12,13 @@ public:
     ~GameServer();
     virtual bool init();
     CREATE_FUNC(GameServer);
+    static void sendGameMsg();
+    static void recvGameMsg(ODSocket* clientSocket);
 
 private:
     ODSocket* mSocket;
-    //Vector<ODSocket*> connectSocket;
-    std::string ip;
+    std::vector<ODSocket*> connectSocket;
+    char ip[101];
     int port;
 };
 #endif
